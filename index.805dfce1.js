41ab9594ef380a2cdcb502308366eb9d9ec277f9
Mateusz Oślizło <amti4osl@gmail.com>
2024-02-07 10:48:20 +0100

    make build

---
+++ index.805dfce1.js
@@ -0,0 +1,2 @@
+document.getElementById("message").addEventListener("submit",function(e){e.preventDefault()}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.805dfce1.js.map
